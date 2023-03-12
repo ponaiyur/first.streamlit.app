8959244eeda8ca07a92cba373feb57ba424754ef
ponaiyur <127597719+ponaiyur@users.noreply.github.com>
2023-03-12 16:59:31 +0530

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -41,7 +41,7 @@ except URLError as e:
   
 #streamlit.stop()
 def get_fruit_load_list():
-    with my_cnx.cursor as my_cur:
+    with my_cnx.cursor() as my_cur:
         my_cur.execute("select * from PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
         return my_cur.fetchall()
 
